8df946fc17d36ff12d51a68517f4160cd4f8237c
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-04-11 16:33:05 +0000

    Removal of Amount Of Options

--- src/terminal/ConfHandler.ts
+++ src/terminal/ConfHandler.ts
@@ -1,17 +1,16 @@
 //config handler
-import TConf from '../data/config/TConf.json';
 import { Logger } from '../NewLogger';
-import { ReturnFields, ReturnOptionName, ReturnOptionState, ReturnOptionValue, ReturnFuncPath } from '../Returner';
+import path from 'path';
+import fs from 'fs';
 
 export async function InitFunctions() 
 {
-    for(let uh = 1; uh < TConf.amount + 1; uh++) //the config amount field is really painful, i need to see when i can remove it to find a better way
+    var the = path.join(__dirname, 'functions');
+    const functionFiles = fs.readdirSync(the).filter(file => file.endsWith(".ts"));
+    for(const file of functionFiles)
     {
-        var JsonFields = ReturnFields("TerminalConfig", uh);
-        var OptionFunc = ReturnFuncPath(JsonFields);
-
-        const optFunc = require(`./functions/${OptionFunc}`);
+        var the2 = path.join(the, file);
+        const optFunc = require(the2);
         optFunc.exec();
-
-    }    
+    }
 }
