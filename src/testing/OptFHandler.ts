7e17f48eac12b0364a6e9affb5f1db034f6f4d00
SanicBTW <58010536+SanicBTW@users.noreply.github.com>
2022-06-14 13:59:59 +0000

    update optf handler

--- src/testing/OptFHandler.ts
+++ src/testing/OptFHandler.ts
@@ -1,16 +1,16 @@
 //for the .optf (option file) file extension
 import fs from 'fs';
-import { coolTextFile } from '../Helper';
 import { Logger } from '../NewLogger';
 
 export var configuration:[{
+    optType: string | null,
     optName: string | null, 
-    optState: string | null
+    optState: string | number | boolean | null //these are quite useless, as we are reading a file but we dont know what type this thing is
 }];
 
-export async function setupOptionFile(path:string)
+export async function setupOptionFile(path:string,)
 {
-    configuration = [{optName: null, optState: null}]
+    configuration = [{optType: null, optName: null, optState: null}]
     var daList:Array<string> = [];
     if(path.endsWith(".optf"))
     {
@@ -18,16 +18,19 @@ export async function setupOptionFile(path:string)
         for(var i in daList)
         {
             var options = daList[i].split(":");
-            configuration.push({optName: options[0], optState: options[1]})
-            configuration[i].optName = options[0];
+            var type = daList[i].split("|");
+            configuration.push({optType: type[0], optName: options[0].replace(type[0] + "|", ""), optState: options[1]});
+            configuration[i].optType = type[0];
+            configuration[i].optName = options[0].replace(type[0] + "|", "");
             configuration[i].optState = options[1];
             //the last item is repeated for some reason
+            configuration.at(-1)!.optType = null;
             configuration.at(-1)!.optName = null;
             configuration.at(-1)!.optState = null;
         }
     }
     else
     {
-        Logger("not an option file", "INFO");
+        Logger("not an option file", "ERROR");
     }
 }
